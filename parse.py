5235e6acc965d938e72249bcf6d3a7261a194569
Christian Torralba <torral2@ada.cs.pdx.edu>
2024-02-29 15:12:51 -0800

    Made a python version. WHY IS IT ALMOST TWICE AS FAST????

---
+++ parse.py
@@ -0,0 +1,34 @@
+import re
+
+def load_variables():
+    file = open("variables.txt", "r")
+    ret_list = []
+    lines = file.readlines()
+    for line in lines:
+        line = line.strip()
+        if line != "":
+            formated_line = f'^;{line}:.*'
+            ret_list.append(formated_line)
+            print(formated_line)
+    return ret_list
+
+
+
+
+def main():
+    file = open("example.gcode", "r")
+    lines = file.readlines()
+    variables = load_variables()
+    for line in lines:
+        for var in variables:
+            x = re.search(var, line)
+            if x != None:
+                print(f'Match found: {x}')
+
+
+
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
